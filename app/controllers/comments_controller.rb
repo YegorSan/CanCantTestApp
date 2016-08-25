5af69f7eb5254acd54fb2a101ad42de6c66d30cd
Yegor San <egorgolovan89@gmail.com>
2016-08-25 21:01:20 +0300

    Posts with CanCan abilities added

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+    
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.build(comment_params)
+        @comment.user = current_user
+        @comment.save
+    end
+    
+    def destroy
+       
+     @post = Post.find(params[:post_id])
+     @comment = @post.comments.find(params[:id]).destroy
+     redirect_to posts_url
+        
+    end
+    
+    private
+    
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+        
+    end
